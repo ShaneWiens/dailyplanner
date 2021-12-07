f46e19f14e30c9d94aae0698ee1fd058cce19323
Shane Wiens <Shanewiens@gmail.com>
2021-12-06 22:29:47 -0800

    So very irritated with myself. Been working on it all day. Worked with groupmates before and after class. FORGOT TO PUSH ANY CHANGES TODAY
    I am so disaapoint...

--- script.js
+++ script.js
@@ -1,14 +1,88 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+console.log("look, I can still link a file!")
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var currentDay = $("#currentDay");
 
-  passwordText.value = password;
+var savedTasks = localStorage.getItem("dailyTasks");
+
+var dailyTasks;
+
+if (!savedTasks) {
+    dailyTasks = {};
+} else {
+    dailyTasks = JSON.parse(savedTasks);
+}
+
+currentDay.text(moment().format('MMMM Do, YYYY'));
+
+fillTimes();
+
+
+function generateHourCards(rowIndex, currentHour) {
+
+    var row = $("<div></div>");
+
+    row.addClass("row");
+
+    if (rowIndex === currentHour) {
+        row.addClass("present");
+    } else if (rowIndex > currentHour) {
+        row.addClass("future");
+    } else {
+        row.addClass("past");
+    }
+
+    var form = $("<textarea></textarea");
+
+    form.addClass("col-9");
+
+    form.text(dailyTasks[rowIndex]);
+
+    var time = $("<div></div>");
+
+    time.addClass("hour col-2");
+
+
+    var buttonContainer = $("<div></div>");
+
+    buttonContainer.addClass("saveBtn col-1");
+
+    var saveIcon = $("<i></i>");
+
+    buttonContainer.append(saveIcon);
+    saveIcon.addClass("fas fa-save");
+    saveIcon.attr("id", rowIndex);
+
+
+    var timeBlock = $("#hourcard");
+
+    timeBlock.append(row);
+    row.append(buttonContainer);
+    row.append(form);
+    row.append(time);
+
+    time.text(`${rowIndex}:00`);
+
+
+    saveIcon.on("click", saveTask);
+}
+
+function fillTimes() {
+
+
+    var currentHour = parseInt(moment().format('H'));
+
+    for (let index = 9; index <= 17; index++) {
+        generateHourCards(index, currentHour);
+    }
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function saveTask(event) {
+    console.log("Saved!");
+
+    var clickedButton = $(event.target);
+    var hour = clickedButton.attr("id");
+    var textValue = clickedButton.parent().siblings("textarea").val();
+    dailyTasks[hour] = textValue;
+    localStorage.setItem("dailyTasks", JSON.stringify(dailyTasks));
+}
